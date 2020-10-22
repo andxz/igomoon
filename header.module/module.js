d1a2da46d9b11c293a013857cd7b3ec01984f151
andxz <andrez.romero@hotmail.com>
2020-10-22 17:03:50 +0200

    added responsivie design and fixed menu

--- header.module/module.js
+++ header.module/module.js
@@ -2,6 +2,6 @@ $('.mobile-toggle').on('click', function() {
   $('html').toggleClass('menu-open');
 });
 
-$('.hide-kids').on('click', function() {
-  $('.hs-menu-item .hs-menu-depth-1 .hs-item-has-children').toggleClass('show-kids');
-});
+// $('.burger-menu li.hs-menu-item.hs-menu-depth-1.hs-item-has-children a').on('click', function() {
+//   $('.hs-menu-children-wrapper').toggleClass('open-child-menu');
+// });
