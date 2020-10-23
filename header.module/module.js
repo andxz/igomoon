2d43efacc9a227ccee6817a3008c6da2c519961e
andxz <andrez.romero@hotmail.com>
2020-10-23 12:41:12 +0200

    code cleaning

--- header.module/module.js
+++ header.module/module.js
@@ -1,7 +1,3 @@
 $('.mobile-toggle').on('click', function() {
   $('html').toggleClass('menu-open');
 });
-
-// $('.burger-menu li.hs-menu-item.hs-menu-depth-1.hs-item-has-children a').on('click', function() {
-//   $('.hs-menu-children-wrapper').toggleClass('open-child-menu');
-// });
